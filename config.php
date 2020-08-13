a4eb2643c6dd738c8984a0be992053438ee06729
zahralakani <69416311+zahralakani@users.noreply.github.com>
2020-08-14 00:05:27 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "*******";
+$username = "*****";
+$password = "******";
+$db = "Tasks";
+
+$conn = mysqli_connect($servername, $username, $password);
+mysqli_select_db($conn, $db);
+?>
